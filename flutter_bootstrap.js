27d905ff2c63f7dbdb2e27f01eb87a257de34e36
Myster7494 <Myster7494@users.noreply.github.com>
2024-06-15 13:45:29 +0000

    deploy: 9dab2081b005c1b47eba4adfa4d2caaea0994ef1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "444687617"
+    serviceWorkerVersion: "1042252366"
   }
 });
